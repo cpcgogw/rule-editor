59d5bf1185af20a8caada9ca3d2bd9f93b5f440d
Oskar Jedvert <oskar.jedvert@gmail.com>
2017-01-24 12:14:27 +0100

    adds basic canvas and buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import static sample.Controller.tools.EDGE;
+import static sample.Controller.tools.NODE;
+
 public class Controller {
+    /**
+     * Buttons for handling active tools
+     */
+    @FXML
+    private Button edge_button;
+    @FXML
+    private Button node_button;
+
+    /**
+     * Enum to keep track of which tool is active
+     */
+    public enum tools {
+        EDGE, NODE, DELETE
+    }
+    private tools activeTool;
+    public void initialize(){
+        activeTool = NODE;
+        node_button.setOnMouseClicked(mouseEvent -> activeTool = NODE);
+        edge_button.setOnMouseClicked(mouseEvent -> activeTool = EDGE);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("canvas.fxml"));
+
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
     }
 
